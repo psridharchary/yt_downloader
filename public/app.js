092c670978a6dd84c359cdf66dc99d9a039529ba
Sridhar Pendota <sridharpendota27@gmail.com>
2021-01-08 19:57:28 +0530

    adding public and node modules folder

---
+++ public/app.js
@@ -0,0 +1,9 @@
+Ext.onReady(function() {
+    Ext.create('Ext.Panel', {
+       renderTo: 'helloWorldPanel',
+       height: 200,
+       width: 600,
+       title: 'Hello world',
+       html: 'First Ext JS Hello World Program'
+    });
+ });
